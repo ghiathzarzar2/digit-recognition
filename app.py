7720cea4919a667057ba582e93013cce20effe91
ghiathzarzar2 <ghiath.zarzar@gmail.com>
2026-07-11 22:50:10 +0300

    Update App to be Able to be Deployed

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 import base64
 import io
+import os
+
 
 app = Flask(__name__)
 CORS(app)  # allows the frontend (different origin) to talk to this backend
@@ -71,5 +73,7 @@ def health_check():
     return jsonify({"status": "Backend is running"})
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
